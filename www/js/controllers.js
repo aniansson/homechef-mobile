990ceefa321fb1e2a42996a9dbc5b421a80acb98
Philip <Philip.zdb@gmail.com>
2017-02-08 16:37:23 +0100

    Adds action to retrieve dishes in DishesCtrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,49 +1,58 @@
 angular.module('homechef.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $ionicLoading, $timeout) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $ionicLoading, $timeout) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
 
-  // Form data for the login modal
-  $scope.loginData = {};
+    // Form data for the login modal
+    $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function (modal) {
+      $scope.modal = modal;
+    });
+
+    // Triggered in the login modal to close it
+    $scope.closeLogin = function () {
+      $scope.modal.hide();
+    };
+
+    // Open the login modal
+    $scope.login = function () {
+      $scope.modal.show();
+    };
+
+    // Perform the login action when the user submits the login form
+    $scope.doLogin = function () {
+      console.log('Doing login', $scope.loginData);
+
+      // Simulate a login delay. Remove this and replace with your login
+      // code if using a login system
+      $timeout(function () {
+        $scope.closeLogin();
+      }, 1000);
+    };
+  })
+
+
+  .controller('DishesCtrl', function ($scope, dishes) {
+    $scope.retrieveDishes = function () {
+      $ionicLoading.show({
+        template: 'Retrieving delicious food'
+      });
+      dishes.query({}, function (response) {
+        $state.go('app.data', {savedDataCollection: response.entries})
+        $ionicLoading.hide();
+      }, function (error) {
+        $ionicLoading.hide();
+        $scope.showAlert('Failure', error.statusText);
+      })
+    };
   });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
-
-.controller('DishesCtrl', function($scope) {
-  $scope.retrieveData = function() {
-
-  }
-});
