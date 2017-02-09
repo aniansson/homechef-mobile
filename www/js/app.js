0695913aefa2b52e88874dfae45c1d24c61bf1b9
Philip <Philip.zdb@gmail.com>
2017-02-09 12:19:52 +0100

    Includes ng-token-auth in module

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'homechef.controllers' is found in controllers.js
-angular.module('homechef', ['ionic', 'homechef.controllers'])
+angular.module('homechef', ['ionic', 'homechef.controllers', 'ng-token-auth'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
