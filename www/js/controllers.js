c401bdd2430616b62e6894d4da007bc4c7c6f7a1
Philip <Philip.zdb@gmail.com>
2017-02-09 17:18:16 +0100

    Fixes error messages, adds close button to login/signup view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('homechef.controllers', [])
 
-  .controller('AppCtrl', function($scope, $ionicModal, $ionicLoading, $timeout, $rootScope, $auth, $ionicPopup) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $ionicLoading, $timeout, $rootScope, $auth, $ionicPopup) {
 
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
@@ -17,40 +17,40 @@ angular.module('homechef.controllers', [])
     // Create the login modal that we will use later
     $ionicModal.fromTemplateUrl('templates/login.html', {
       scope: $scope
-    }).then(function(modal) {
+    }).then(function (modal) {
       $scope.loginmodal = modal;
     });
 
     $ionicModal.fromTemplateUrl('templates/signup.html', {
       scope: $scope
-    }).then(function(modal) {
+    }).then(function (modal) {
       $scope.signupmodal = modal;
     });
 
     // Triggered in the login modal to close it
-    $scope.closeLogin = function() {
+    $scope.closeLogin = function () {
       $scope.loginmodal.hide();
     };
 
-    $scope.closeSignup = function() {
+    $scope.closeSignup = function () {
       $scope.signupmodal.hide();
     };
 
     // Open the login modal
-    $scope.login = function() {
+    $scope.login = function () {
       $scope.loginmodal.show();
     };
 
-    $scope.signup = function() {
+    $scope.signup = function () {
       $scope.signupmodal.show();
     };
 
-    $scope.doSignup = function() {
+    $scope.doSignup = function () {
       $ionicLoading.show({
         template: 'Signing up...'
       });
       $auth.submitRegistration($scope.signupData)
-        .then(function(resp) {
+        .then(function (resp) {
           $ionicLoading.hide();
           $scope.closeSignup();
           $ionicLoading.show({
@@ -58,20 +58,19 @@ angular.module('homechef.controllers', [])
             duration: 1000
           });
         })
-        .catch(function(error) {
+        .catch(function (response) {
           $ionicLoading.hide();
-          console.log(error);
-          $scope.errorMessage = error;
-          $scope.showAlert('Failure', 'Invalid email or password');
+          $scope.errorMessage = response;
+          $scope.showAlert('Failure', response.data.errors.full_messages);
         });
     };
     // Perform the login action when the user submits the login form
-    $scope.doLogin = function() {
+    $scope.doLogin = function () {
       $ionicLoading.show({
         template: 'Logging in...'
       });
       $auth.submitLogin($scope.loginData)
-        .then(function(resp) {
+        .then(function (resp) {
           $ionicLoading.hide();
           $scope.closeLogin();
           $ionicLoading.show({
@@ -79,54 +78,54 @@ angular.module('homechef.controllers', [])
             duration: 1000
           });
         })
-        .catch(function(error) {
+        .catch(function (response) {
           $ionicLoading.hide();
-          $scope.errorMessage = error;
-          $scope.showAlert('Failure', 'Invalid email or password');
+          $scope.errorMessage = response;
+          $scope.showAlert('Failure', response.data.errors.full_messages);
         });
-      };
+    };
 
 
+    $scope.showAlert = function (message, content) {
+      var alertPopup = $ionicPopup.alert({
+        title: message,
+        template: content
+      });
+      alertPopup.then(function (res) {
 
-      $scope.showAlert = function(message, content) {
-        var alertPopup = $ionicPopup.alert({
-          title: message,
-          template: content
-        });
-        alertPopup.then(function(res) {
+      });
+    };
 
-        });
-      };
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function () {
+      $scope.closeLogin();
+    }, 1000);
 
-      // Simulate a login delay. Remove this and replace with your login
-      // code if using a login system
-      $timeout(function() {
-        $scope.closeLogin();
-      }, 1000);
-
-    $rootScope.$on('auth:login-success', function(ev, user) {
+    $rootScope.$on('auth:login-success', function (ev, user) {
       $scope.currentUser = angular.extend(user, $auth.retrieveData('auth_headers'));
     });
 
-    $rootScope.$on('auth:registration-email-success', function(ev, user) {
+    $rootScope.$on('auth:registration-email-success', function (ev, user) {
       console.log("hi");
       $scope.currentUser = angular.extend(user, $auth.retrieveData('auth_headers'));
     });
   })
 
 
-  .controller('dishesCtrl', function($state, $scope, dishes, $ionicLoading, $ionicPopup) {
-    $scope.$on('$ionicView.enter', function() {
+  .controller('dishesCtrl', function ($state, $scope, dishes, $ionicLoading, $ionicPopup) {
+    $scope.$on('$ionicView.enter', function () {
       $ionicLoading.show({
         template: 'Retrieving delicious food'
       });
-      dishes.query(function(response) {
+      dishes.query(function (response) {
         $scope.dishCollection = response.entries;
         $ionicLoading.hide();
-      }, function(response) {
+      }, function (response) {
         //error
       });
     });
   })
 
-  .controller('displayDishCtrl', function($scope, $stateParams) {});
+  .controller('displayDishCtrl', function ($scope, $stateParams) {
+  });
